4a0ddacc7928e40abcf95b65673702b0e8ad10ea
SaranshDawra <saransh2000dawra@gmail.com>
2020-09-25 18:44:12 +0530

    components added

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Hero from './Hero/Hero';
+import LearnSection from './LearnSection/LearnSection';
+import CodeSection from './CodeSection/CodeSection';
+import GetHiredSection from './GetHiredSection/GetHiredSection';
 
 class Landing extends Component {
     render() {
         return (
             <div>
                 <Hero />
+                <LearnSection />
+                <CodeSection />
+                <GetHiredSection />
             </div>
         )
     }
