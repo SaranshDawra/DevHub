729d8f5753cdad98bf85577356af1a1ba73175f3
SaranshDawra <saransh2000dawra@gmail.com>
2020-10-02 23:08:09 +0530

    Get Hired page

--- client/src/components/GetHired/GetHired.js
+++ client/src/components/GetHired/GetHired.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import JobCard from "./JobCard/JobCard";
+import Loader from "../UI/Loader/Loader";
+import classes from "./gethired.module.css";
 
 export default class GetHired extends Component {
+    state = {
+        jobs: [],
+        isLoading: true,
+    };
+
+    componentDidMount() {
+        axios
+            .get("/api/jobs")
+            .then((res) => {
+                // console.log(res.data);
+                this.setState({
+                    jobs: res.data,
+                });
+            })
+            .catch((err) => console.log(err));
+    }
+
+    componentDidUpdate() {
+        if(this.state.isLoading) {
+            this.setState({
+                isLoading: false
+            })
+        }
+    }
+
     render() {
+        let element = <Loader />;
+
+        if (!this.state.isLoading) {
+            element = this.state.jobs.map((job) => {
+                return <JobCard key={job.id} {...job} />;
+            });
+        }
+
         return (
-            <h1 style={{paddingTop: '100px'}}>Get Hired</h1>
-        )
+            <div className={classes.Container}>
+                <div className={classes.InnerContainer}>
+                    {element}
+                </div>
+            </div>
+        );
     }
 }
