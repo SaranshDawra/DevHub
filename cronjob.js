f8cf6193423004a9b0e50263aaefd97da71e3ba4
SaranshDawra <saransh2000dawra@gmail.com>
2020-10-10 15:47:35 +0530

    cronjob added

---
+++ cronjob.js
@@ -0,0 +1,23 @@
+const Code = require("./models/code");
+const Question = require('./models/question');
+const cronJob = require("node-cron");
+const mongoose = require("mongoose");
+
+const dailyCode = () => {
+    cronJob.schedule("59 23 * * * ", async () => {
+        const code = await Code.find();
+        const questions = await Question.find();
+        const question = questions[ (code[0].count + 1) % questions.length];
+        await Code.findByIdAndUpdate(
+            code[0]._id,
+            {
+                code: question,
+                count: (code[0].count + 1) % questions.length
+            },
+            { new: true }
+        );
+        console.log('Job Completed');
+    });
+};
+
+module.exports = dailyCode;
