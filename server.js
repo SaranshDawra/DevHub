861ee04e8c0d9b33eb8f462a44245c2928ee3a5f
SaranshDawra <saransh2000dawra@gmail.com>
2020-10-10 15:51:44 +0530

    routes added

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
-const courses = require('./routes/courses');
-const jobs = require('./routes/jobs');
+const courses = require("./routes/courses");
+const jobs = require("./routes/jobs");
+const questions = require("./routes/questions");
+const codes = require("./routes/codes");
+const dailyCode = require("./cronjob");
+const path = require('path');
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const uri = process.env.MONGODB_URI;
 
@@ -14,12 +20,25 @@ mongoose
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
+        useFindAndModify: true,
     })
-    .then(() => console.log("MongoDB Connected."))
+    .then(() => {
+        console.log("MongoDB Connected.")
+        dailyCode();
+    })
     .catch((err) => console.log(err));
 
-app.use('/api/courses', courses);
-app.use('/api/jobs', jobs);
+app.use("/api/courses", courses);
+app.use("/api/jobs", jobs);
+app.use("/api/questions", questions);
+app.use("/api/codes", codes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
